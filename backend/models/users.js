0f05a4251ee3e52bd7bd7c0264a7d487ec82cdcd
Shivam16a <shivam123hjp@gmail.com>
2026-02-11 22:32:53 +0530

    user model creation done

--- backend/models/users.js
+++ backend/models/users.js
@@ -3,7 +3,37 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username:{
         type:String,
-        require:true,
+        required:true,
         trim:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    prn:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    profilepic:{
+        type:String
+    },
+    gender:{
+        type:String,
+        enum:["male","female"],
+    },
+    discriptin:{
+        type:String
+    },
+    password:{
+        type:String,
+        required:true
     }
-})
+},{timeseries:true});
+
+module.exports = mongoose.model('User',userSchema);
+
